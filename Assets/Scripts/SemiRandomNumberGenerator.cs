3e30d807eac7116f8ecad3a944955a063aa19fca
phy5prt <philip.r.tate@gmail.com>
2018-10-30 22:35:02 +0000

    i dont know why i am so pleased with my randomnumber generator but I am

--- Assets/Scripts/SemiRandomNumberGenerator.cs
+++ Assets/Scripts/SemiRandomNumberGenerator.cs
@@ -7,6 +7,9 @@ public class SemiRandomNumberGenerator : MonoBehaviour {
 //i want to make a random number that create another random number near to itself
 //with a bias i can alter so they move up and down
 
+
+//think it leaks up but hmm 
+
 [Range( 2,100)]
 	[SerializeField] int roundnessBellCurveInRNAddedRange= 2;
 
@@ -32,7 +35,7 @@ private float rNAdded;
 [Range( 0f,0.1f)]  // so if set to ).2f that means if direction of bias is 1 the max increase step will be 20% of current position with bell point at 10%
 	[SerializeField] float rNAddedRange = 0.1f;
 private float	rNAddedRangeLow;
-private float	NAddedRangeHigh;
+private float	rNAddedRangeHigh;
 	// Use this for initialization
 	void Start () {
 	theRandomNumber = startingPlace;	
@@ -53,7 +56,7 @@ private float	NAddedRangeHigh;
 	{
 	float rangeBell = 0;
 		for(int i = 0; i <roundnessBellCurveInRNAddedRange; i++){
-			rangeBell += Random.Range (rNAddedRangeLow, NAddedRangeHigh) /roundnessBellCurveInRNAddedRange ;
+			rangeBell += Random.Range (rNAddedRangeLow, rNAddedRangeHigh) /roundnessBellCurveInRNAddedRange ;
 		}
 
 		//TODO graph this!
@@ -62,10 +65,10 @@ private float	NAddedRangeHigh;
 
 	private float createRandomNumber ()
 	{
-		rNAddedRangeLow = rNAddedRange + directionBiasOfRn * rNAddedRange;
-		NAddedRangeHigh = -rNAddedRange + directionBiasOfRn * rNAddedRange;
+		rNAddedRangeLow = -1*rNAddedRange + directionBiasOfRn * rNAddedRange;
+		rNAddedRangeHigh = +rNAddedRange + directionBiasOfRn * rNAddedRange;
 		rNAdded = makeRangeBell ();
-		theRandomNumber = theRandomNumber + rNAddedRange * Time.deltaTime * sensitivityToRateOfUpdate;
+		theRandomNumber = theRandomNumber + rNAdded * Time.deltaTime * sensitivityToRateOfUpdate;
 		//catching it getting out of bound
 		if (theRandomNumber < 0) {
 			theRandomNumber = 0;
