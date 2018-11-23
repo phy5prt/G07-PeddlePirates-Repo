8e9ac4492e9946d56921272e6626d7d09596bb1d
phy5prt <philip.r.tate@gmail.com>
2018-11-23 18:02:41 +0000

    added some temporoary ship light till got itme to do nice ones also im about to delete max and current texts where not useful

--- Assets/Scripts/percBarDisplay.cs
+++ Assets/Scripts/percBarDisplay.cs
@@ -6,17 +6,20 @@ using UnityEngine.UI;
 
 public class percBarDisplay : MonoBehaviour {
 
-//I am leftBool ?
+
 //does it find its partner and work it out that way with local transform
 
-public GameObject GetVolt;
-public float voltCurrent;
-public float voltPerc;
-public float voltMax;
+
+private float voltCurrent;
+private float voltPerc;
+
 private Image percBar;
-public float proportion200Bar;
-public Text CurrentHighestVolt;
-public Text CurrentVoltOutput;
+private float proportion200Bar;
+private Text CurrentHighestVolt;
+private Text CurrentVoltOutput;
+[SerializeField] bool leftSideBar = true; //would be better if set self in code rather than me saying if left
+private thisPlayerPairSettings myPlayerPairSettings;
+
 
 
 	// Use this for initialization
@@ -29,21 +32,18 @@ public Text CurrentVoltOutput;
 	}
 	void Update () {
 
+	if(myPlayerPairSettings != null){
+
+	if(leftSideBar){voltCurrent = myPlayerPairSettings.GetmyLeftVolt();}else{voltCurrent = myPlayerPairSettings.GetmyRightVolt();} // if using get set could just say getset
 	CurrentVoltOutput.text = "Current Volts: " + "\r\n" + voltCurrent; //if too fast put on coroutine
 
-	voltCurrent = GetVolt.GetComponent<GetBikeFloatsHere>().bikeVolts;
 
-	if(voltCurrent>voltMax){
-	voltMax=voltCurrent*1.3f;
-	CurrentHighestVolt.text = "Max Volt Set: " + "\r\n" + voltMax;
 
-	} //it should delay displaying this so can exceded previous
 
-	//should display volt number
-	//or could reach the top95 % change colour when you hit it
-	//line shows when you reach 90%
 
-	voltPerc = voltCurrent/voltMax;
+
+
+	voltPerc = voltCurrent/myPlayerPairSettings.GetVolt100Perc();
 	proportion200Bar =	voltPerc; //so can get up to 90% of bar
 	percBar.fillAmount = proportion200Bar; //goes up to 200% at moment hence divide by two
 
@@ -52,41 +52,16 @@ public Text CurrentVoltOutput;
 	if(proportion200Bar <  0.8 && proportion200Bar>=0.7){percBar.color = Color.green;} 
 	if(proportion200Bar <  0.7 && proportion200Bar>=0.5){percBar.color = Color.yellow;} 
 	if(proportion200Bar <  0.5){percBar.color = Color.red;} 
-
+	}
 
 	}
 
-	/* for comparison
+	public void passMeMyPlayerPairSettings(thisPlayerPairSettings barsPlayerPairSettings){
 
-	void powerBarDisplayAndSetValues ()
-	{
-		CurrentVoltOutput.text = "Current Volts: " + "\r\n" + pairVoltCurrent;
+	myPlayerPairSettings = barsPlayerPairSettings;
 
-
-		voltCurrentL = myThisPlayerPairSettings.GetmyLeftVolt();
-		voltCurrentR = myThisPlayerPairSettings.GetmyRightVolt();
-		pairVoltCurrent = (voltCurrentL + voltCurrentR)/2;
-		if (voltMax > 0) {
-			voltPerc = pairVoltCurrent / voltMax;
-		}
-		proportion200Bar = voltPerc;
-		//so can get up to 90% of bar
-		percBar.fillAmount = proportion200Bar;
-		//goes up to 200% at moment hence divide by two
-		if (proportion200Bar >= 0.9) {
-			percBar.color = Color.cyan;
-		}
-		if (proportion200Bar < 0.8 && proportion200Bar >= 0.7) {
-			percBar.color = Color.green;
-		}
-		if (proportion200Bar < 0.7 && proportion200Bar >= 0.5) {
-			percBar.color = Color.yellow;
-		}
-		if (proportion200Bar < 0.5) {
-			percBar.color = Color.red;
-		}
 	}
 
-	*/
+
 
 }
