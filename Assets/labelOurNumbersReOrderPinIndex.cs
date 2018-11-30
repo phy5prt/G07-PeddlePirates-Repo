bb6bcee776e90c463b6b7c3565d9447d47dbb23d
phy5prt <philip.r.tate@gmail.com>
2018-11-30 23:22:27 +0000

    struggling to work how to in real time swop images around seems to be no direct way to do it short of deleting list and remaking it which in a way is fine seeing i want my items text just to be its index and im changing the image anyway so the item list will literally just be each sprite and its array

--- Assets/labelOurNumbersReOrderPinIndex.cs
+++ Assets/labelOurNumbersReOrderPinIndex.cs
@@ -14,11 +14,48 @@ public class labelOurNumbersReOrderPinIndex : MonoBehaviour {
 	//and staticly run the method for updating the array
 
 	public Sprite[] bikeIcons;
+	private int[] selectedOption;
+	private Sprite[] newOptionsSprites;
+	private Dropdown[] dropDowns;
+
+	void Start(){
+
+	dropDowns = GetComponentsInChildren<Dropdown>();
+
+
+	}
+
 
 	public void updateTheIndexAndImageLabels(){
 
+	//needs to find all the options in each  drop down and change 
 	//if arduino static could directly send the need index array
 	//apply images 
+
+	//selected opt
+
+	for(int i =0; i<8; i++){selectedOption[i] = dropDowns[i].value;}
+
+
+	for (int i =0; i<8; i++){newOptionsSprites[i] = bikeIcons[8];}
+
+	for(int i =0; i<8; i++){newOptionsSprites[selectedOption[i]]=bikeIcons[i];}
+
+	for(int i = 0; i<8; i++){
+	for(int j = 0; j<8; j++){
+
+	//do i have to just make new list not assign
+	//dropDowns[i].; //options. = newOptionsSprites[i];
+
+
+				//https://www.youtube.com/watch?v=LRoqGsJGgA4
+	}
+	}
+		//https://gamedev.stackexchange.com/questions/115292/how-do-i-remove-specfic-optiondata-from-dropdown
+
+
+
+
 
 	}
 }
