a856cbfcbaebb9593b17d8b37c98e2c6d1bff9a5
phy5prt <philip.r.tate@gmail.com>
2018-12-01 12:41:31 +0000

    all but one box seems to get its image bizarly but works otherwise

---
+++ Assets/dropDownEventScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dropDownEventScript : MonoBehaviour {
+
+private Dropdown thisDropDown;
+private Dropdown[] dropDowns = new Dropdown[8];
+private labelOurNumbersReOrderPinIndex reorder;
+	// Use this for initialization
+	void Start () {
+		thisDropDown = GetComponent<Dropdown>();
+		thisDropDown.onValueChanged.AddListener(delegate{DropdownValueChanged(thisDropDown);});
+		dropDowns = this.transform.parent.gameObject.GetComponentsInChildren<Dropdown>();
+		reorder =  this.transform.parent.gameObject.GetComponent<labelOurNumbersReOrderPinIndex>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void DropdownValueChanged(Dropdown change){
+	Debug.Log("a drop down option changed");
+	for(int i =0; i<8;i++){
+	if((dropDowns[i] != thisDropDown) &&(dropDowns[i].value == thisDropDown.value)){dropDowns[i].value = 8;} //set those sharing number to blank
+	}
+
+	reorder.updateTheIndexAndImageLabels();
+	}
+}
--- Assets/labelOurNumbersReOrderPinIndex.cs
+++ Assets/labelOurNumbersReOrderPinIndex.cs
@@ -5,6 +5,10 @@ using UnityEngine.UI;
 
 public class labelOurNumbersReOrderPinIndex : MonoBehaviour {
 
+	//https://www.youtube.com/watch?v=LRoqGsJGgA4
+	//https://gamedev.stackexchange.com/questions/115292/how-do-i-remove-specfic-optiondata-from-dropdown
+
+
 	//this code will change the images next to the option
 	//this script used multiple times
 	//would be nice if we had all drop downs pulling from one template and update that
@@ -13,16 +17,18 @@ public class labelOurNumbersReOrderPinIndex : MonoBehaviour {
 	//should the holding object have these as an array and be triggered when they change to collect all there values
 	//and staticly run the method for updating the array
 
-	public Sprite[] bikeIcons;
-	private int[] selectedOption;
-	private Sprite[] newOptionsSprites;
+	public Sprite[] bikeIcons = new Sprite[9];
+	private int[] selectedOption = new int[9];
+	private Sprite[] newOptionsSprites= new Sprite[9]; //should it be 9 with the blank
 	private Dropdown[] dropDowns;
+	private List<Dropdown.OptionData> dropDownOptionsTxtsSprites;
 
 	void Start(){
 
 	dropDowns = GetComponentsInChildren<Dropdown>();
+	Debug.Log("I found " +dropDowns.Length+ " dropdowns");
 
-
+		updateTheIndexAndImageLabels();
 	}
 
 
@@ -34,28 +40,39 @@ public class labelOurNumbersReOrderPinIndex : MonoBehaviour {
 
 	//selected opt
 
+	//list of the value on all 8 drop downs
 	for(int i =0; i<8; i++){selectedOption[i] = dropDowns[i].value;}
 
+	//make all the sprites to the blank incon
+	for (int i =0; i<9; i++){newOptionsSprites[i] = bikeIcons[8];}
 
-	for (int i =0; i<8; i++){newOptionsSprites[i] = bikeIcons[8];}
-
-	for(int i =0; i<8; i++){newOptionsSprites[selectedOption[i]]=bikeIcons[i];}
-
-	for(int i = 0; i<8; i++){
-	for(int j = 0; j<8; j++){
-
-	//do i have to just make new list not assign
-	//dropDowns[i].; //options. = newOptionsSprites[i];
+	//reorder sprites based on the value selected in drop downs but dont want to change blank so 8
+	for(int i =0; i<9; i++){
+			if(selectedOption[i]==8){newOptionsSprites[selectedOption[i]]=bikeIcons[8];}else{
+	newOptionsSprites[selectedOption[i]]=bikeIcons[i];}}
 
 
-				//https://www.youtube.com/watch?v=LRoqGsJGgA4
+
+
+	//clearing the list and remaking it
+	foreach(Dropdown drop in dropDowns){ drop.options.Clear();}
+	dropDownOptionsTxtsSprites = new  List<Dropdown.OptionData>();
+
+		//do i have to just make new list not assign
+	//options. = newOptionsSprites[i];
+
+	//making an option
+	//then adding the list
+	for(int j = 0; j<9; j++){
+		Dropdown.OptionData option = new Dropdown.OptionData(j.ToString(), newOptionsSprites[j]);
+		dropDownOptionsTxtsSprites.Add(option);
+		}
+	foreach(Dropdown drop in dropDowns){ drop.AddOptions(dropDownOptionsTxtsSprites);}
 	}
-	}
-		//https://gamedev.stackexchange.com/questions/115292/how-do-i-remove-specfic-optiondata-from-dropdown
+		
 
 
 
 
-
-	}
+	
 }
